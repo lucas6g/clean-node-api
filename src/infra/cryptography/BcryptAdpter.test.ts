376f0d390c36e309598897c94aef383b26601fd2
lucas6g <lucas.trabalho1533@gmail.com>
2021-11-21 16:45:33 -0300

    test: ensure BcryptAdpter returns as hash on success

--- src/infra/cryptography/BcryptAdpter.test.ts
+++ src/infra/cryptography/BcryptAdpter.test.ts
@@ -2,6 +2,14 @@ import { Encrypter } from '../../data/protocols/Encrypter'
 import { BcryptAdpter } from './BcryptAdpter'
 import bcrypt from 'bcrypt'
 
+jest.mock('bcrypt', () => {
+  return {
+    async hash (): Promise<string> {
+      return await new Promise(resolve => resolve('hashedValue'))
+    }
+  }
+})
+
 const makeSut = (salt: number): Encrypter => {
   return new BcryptAdpter(salt)
 }
@@ -14,5 +22,13 @@ describe('BcryptAdpter', () => {
     await sut.encrypt('anyValue')
 
     expect(hashSpy).toHaveBeenCalledWith('anyValue', salt)
+  })
+  test('should return a hash on success', async () => {
+    const salt = 12
+    const sut = makeSut(salt)
+
+    const hashValue = await sut.encrypt('anyValue')
+
+    expect(hashValue).toBe('hashedValue')
   })
 })
