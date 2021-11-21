dc34e26e1b206df537f3cbdd8158fa0e982150c6
lucas6g <lucas.trabalho1533@gmail.com>
2021-11-21 14:16:36 -0300

    test: ensure DbAddAccount calls encrypter whit correct password

---
+++ src/data/usecases/add-account/DbAddAccount.test.ts
@@ -0,0 +1,47 @@
+import { AddAccount } from '../../../domain/usecases/AddAccount'
+import { DbAddAccount } from './DbAddAccount'
+import { Encrypter } from '../../protocols/Encrypter'
+
+const makeEncrypterStub = (): Encrypter => {
+  // dependencia mockada
+  class EncrypterStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return await new Promise(resolve => resolve('hashedPassword'))
+    }
+  }
+  return new EncrypterStub()
+}
+
+interface SutTypes {
+  sut: AddAccount
+  encrypterStub: Encrypter
+
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypterStub()
+  const sut = new DbAddAccount(encrypterStub)
+
+  return {
+    sut,
+    encrypterStub
+
+  }
+}
+
+describe('DbAddAccount', () => {
+  test('should call encrypter whit correct password', async () => {
+    const { sut, encrypterStub } = makeSut()
+    const accountData = {
+      name: 'anyName',
+      email: 'anyEmail@gmail.com',
+      password: 'anyPassword'
+    }
+
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+
+    await sut.add(accountData)
+
+    expect(encryptSpy).toHaveBeenCalledWith('anyPassword')
+  })
+})
