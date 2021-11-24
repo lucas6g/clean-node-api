7910c0b8ed5006557a849846171fdf94c9cf20bc
lucas6g <lucas.trabalho1533@gmail.com>
2021-11-24 08:57:04 -0300

    feat: ensure LogControllerDecorator calls controller whit correct value

---
+++ src/main/decorators/LogDecorator.ts
@@ -0,0 +1,16 @@
+
+import { Controller } from '../../presentation/protocols/Controller'
+import { HttpRequest, HttpResponse } from '../../presentation/protocols/Http'
+
+export class LogControllerDecorator implements Controller {
+  private readonly controller: Controller
+  constructor (controller: Controller) {
+    this.controller = controller
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const httpResponse = await this.controller.handle(httpRequest)
+
+    return httpResponse
+  }
+}
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,15 +2,19 @@ import { DbAddAccount } from '../../data/usecases/add-account/DbAddAccount'
 import { BcryptAdpter } from '../../infra/cryptography/BcryptAdpter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/AccountMongoRepository'
 import { SignupController } from '../../presentation/controllers/SignupController/SignupController'
+import { Controller } from '../../presentation/protocols/Controller'
+
 import { EmailValidatorAdpter } from '../../utils/EmailValidatorAdpter'
+import { LogControllerDecorator } from '../decorators/LogDecorator'
 
 // padra de projeto factory
-export const makeSignupController = (): SignupController => {
+export const makeSignupController = (): Controller => {
   const emailValidatorAdpter = new EmailValidatorAdpter()
   const salt = 12
   const accountMongoRepository = new AccountMongoRepository()
   const bcryptAdpter = new BcryptAdpter(salt)
   const dbAddAccount = new DbAddAccount(bcryptAdpter, accountMongoRepository)
 
-  return new SignupController(emailValidatorAdpter, dbAddAccount)
+  const signUpController = new SignupController(emailValidatorAdpter, dbAddAccount)
+  return new LogControllerDecorator(signUpController)
 }
