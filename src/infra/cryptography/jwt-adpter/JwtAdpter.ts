6342fa0f6eb6d50038142e44a33ce8be13f7eda8
lucas6g <lucas.trabalho1533@gmail.com>
2022-01-04 23:18:58 +0044

    feat: ensure JwtAdpter calls sign whit correct values

---
+++ src/infra/cryptography/jwt-adpter/JwtAdpter.ts
@@ -0,0 +1,16 @@
+
+import { TokenGenerator } from '../../../data/protocols/cryptography/TokenGenerator'
+import { sign } from 'jsonwebtoken'
+
+export class JwtAdpter implements TokenGenerator {
+    private readonly secret: string
+
+    constructor(secret: string) {
+        this.secret = secret
+    }
+
+    async generate(value: string): Promise<string> {
+        sign({ id: value }, this.secret)
+        return await Promise.resolve('')
+    }
+}
