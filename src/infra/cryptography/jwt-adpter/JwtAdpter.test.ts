5b1b6805f53a088e7357234bf6ded7801e90da1f
lucas6g <lucas.trabalho1533@gmail.com>
2022-01-04 23:41:46 +0044

    refactor: move JwtAdpter creation to a factory helper method

--- src/infra/cryptography/jwt-adpter/JwtAdpter.test.ts
+++ src/infra/cryptography/jwt-adpter/JwtAdpter.test.ts
@@ -1,5 +1,6 @@
 import { JwtAdpter } from './JwtAdpter'
 import jwt from 'jsonwebtoken'
+import { TokenGenerator } from '../../../data/protocols/cryptography/TokenGenerator'
 
 jest.mock('jsonwebtoken', () => {
     return {
@@ -9,9 +10,14 @@ jest.mock('jsonwebtoken', () => {
     }
 })
 
+const makeSut = (): TokenGenerator => {
+    const secret = 'secret'
+    return new JwtAdpter(secret)
+}
+
 describe('JwtAdpter', () => {
     test('should call sign whit correct values', async () => {
-        const sut = new JwtAdpter('secret')
+        const sut = makeSut()
 
         const signSpy = jest.spyOn(jwt, 'sign')
         await sut.generate('anyValue')
@@ -19,14 +25,14 @@ describe('JwtAdpter', () => {
         expect(signSpy).toHaveBeenCalledWith({ id: 'anyValue' }, 'secret')
     })
     test('should return a token on sign success', async () => {
-        const sut = new JwtAdpter('secret')
+        const sut = makeSut()
 
         const token = await sut.generate('anyValue')
 
         expect(token).toBe('anyToken')
     })
     test('should trows an error when sign trows an error', async () => {
-        const sut = new JwtAdpter('secret')
+        const sut = makeSut()
 
         jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
             throw new Error()
