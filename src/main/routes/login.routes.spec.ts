2e6e211e8712730a02eb80a8416cd1c755c0ddfd
lucas6g <lucas.trabalho1533@gmail.com>
2022-01-05 23:32:30 +0044

    refactor: move email EmailValidatorAdpter to main layer

--- src/main/routes/login.routes.spec.ts
+++ src/main/routes/login.routes.spec.ts
@@ -3,7 +3,7 @@ import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { app } from '../config/app'
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(String(process.env.MONGO_URL))
   })
@@ -16,14 +16,17 @@ describe('SignUp Routes', () => {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
-  test('should return an account on success', async () => {
-    const response = await request(app).post('/signup').send({
-      name: 'lucas',
-      email: 'lucas@gmail.com',
-      password: '123',
-      passwordConfirmation: '123'
-    })
 
-    expect(response.status).toBe(201)
+  describe('POST /signup', () => {
+    test('should return 201  on signup', async () => {
+      const response = await request(app).post('/signup').send({
+        name: 'lucas',
+        email: 'lucas@gmail.com',
+        password: '123',
+        passwordConfirmation: '123'
+      })
+
+      expect(response.status).toBe(201)
+    })
   })
 })
