37df4e03bfae5ac31cb95e8443d0e13193148b7d
lucas6g <lucas.trabalho1533@gmail.com>
2022-01-09 00:43:17 +0044

    chore: add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoURL: process.env.MONGO_URL ?? 'mongodb://127.0.0.1:27017/gssapiServiceName=mongodb?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false',
+  mongoURL: process.env.MONGO_URL ?? 'mongodb://mongo:27017/clean-node-api',
   port: process.env.PORT ?? 3333,
   jwtSecret: process.env.JWT_SECRET ?? '076cfdef19d43a90dcbcf4d7f13f1f0a82b9eb64'
 }
