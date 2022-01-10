14bf3063c0fd808a1728c4169c84c41908c8f207
lucas6g <lucas.trabalho1533@gmail.com>
2022-01-10 21:58:52 +0044

    feat: ensure DbAddAccount calls Load LoadAccountByEmailRepository whit correct email

--- src/data/usecases/add-account/DbAddAccount.ts
+++ src/data/usecases/add-account/DbAddAccount.ts
@@ -2,19 +2,31 @@
 import { Account } from '../../../domain/entities/Account'
 import { AddAccount, AddAccountModel } from '../../../domain/usecases/AddAccount'
 import { Hasher } from '../../protocols/cryptography/Hasher'
+import { LoadAccountByEmailRepository } from '../authentication/LoadAccountByEmailRepository'
 import { AddAccountRepository } from './AddAccountRepository'
 
 export class DbAddAccount implements AddAccount {
   private readonly hasher: Hasher
   private readonly addAccountRepository: AddAccountRepository
+  private readonly loadAccountByEmailRespository: LoadAccountByEmailRepository
 
-  constructor(hasher: Hasher, addAccountRepository: AddAccountRepository) {
+
+  constructor(
+    hasher: Hasher,
+    addAccountRepository: AddAccountRepository,
+    loadAccountByEmailRespository: LoadAccountByEmailRepository
+  ) {
     this.hasher = hasher
     this.addAccountRepository = addAccountRepository
+    this.loadAccountByEmailRespository = loadAccountByEmailRespository
   }
 
   async add(accountData: AddAccountModel): Promise<Account | null> {
     const hasherPassword = await this.hasher.hash(accountData.password)
+
+    await this.loadAccountByEmailRespository.getByEmail(accountData.email)
+
+
 
     const account = await this.addAccountRepository.save(Object.assign({}, accountData, { password: hasherPassword }))
 
