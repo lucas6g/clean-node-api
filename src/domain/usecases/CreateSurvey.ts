744012f4fecc3afe2550c6ab9392e9950bc3943d
lucas6g <lucas.trabalho1533@gmail.com>
2022-01-14 15:11:41 +0044

    test: ensure CreateSurveyControllers calls CreateSurvey whit correct values

---
+++ src/domain/usecases/CreateSurvey.ts
@@ -0,0 +1,17 @@
+
+export interface CreateSurveyModel {
+    question: string
+    answers: SurveyAnswer[]
+
+}
+export interface SurveyAnswer {
+    image: string
+    answer: string
+
+}
+
+
+
+export interface CreateSurvey {
+    create: (survey: CreateSurveyModel) => Promise<void>
+}
--- src/presentation/controllers/survey/CreateSurveyController/CreateSurveyController.test.ts
+++ src/presentation/controllers/survey/CreateSurveyController/CreateSurveyController.test.ts
@@ -1,6 +1,7 @@
 import { HttpRequest } from "../../../protocols/HttpRequest"
 import { Validation } from "../../../protocols/Validation"
 import { CreateSurveyController } from './CreateSurveyController'
+import { CreateSurvey, CreateSurveyModel } from '../../../../domain/usecases/CreateSurvey'
 
 
 const makeFakeHttpRequest = (): HttpRequest => {
@@ -24,28 +25,42 @@ const makeValidationStub = (): Validation => {
     return new ValidationStub()
 }
 
+const makeSurveyStub = () => {
+    class CreateSurveyStub implements CreateSurvey {
+        async create(survey: CreateSurveyModel): Promise<void> {
+            return Promise.resolve()
+        }
+    }
+    return new CreateSurveyStub()
+}
+
 interface SutTypes {
     sut: CreateSurveyController
-
+    createSurveyStub: CreateSurvey
     validationStub: Validation
 
 }
 const makeSut = (): SutTypes => {
 
     const validationStub = makeValidationStub()
+    const createSurveyStub = makeSurveyStub()
 
-    const sut = new CreateSurveyController(validationStub)
+    const sut = new CreateSurveyController(validationStub, createSurveyStub)
 
     return {
         sut,
         validationStub,
+        createSurveyStub
 
     }
 }
 
 
+
+
+
 describe('CreateSurvey Controller', () => {
-    test('should call call Validation whit correct values', async () => {
+    test('should  call Validation whit correct values', async () => {
         const { sut, validationStub } = makeSut()
 
         const httpRequest = makeFakeHttpRequest()
@@ -69,6 +84,26 @@ describe('CreateSurvey Controller', () => {
 
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new Error())
+    })
+    test('should call  CreateSurvey whit correct values', async () => {
+        const { sut, createSurveyStub } = makeSut()
+
+        const httpRequest = makeFakeHttpRequest()
+
+        const createSurveySpy = jest.spyOn(createSurveyStub, 'create')
+
+        await sut.handle(httpRequest)
+
+
+
+        expect(createSurveySpy).toHaveBeenCalledWith({
+            question: 'anyQuestion',
+            answers: [{
+                image: 'anyImage',
+                answer: 'anyAnswer'
+            }]
+        })
+
     })
 
 })
