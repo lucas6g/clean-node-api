dbfb4d93198a6619094cb8673be8c52dfa21a956
lucas6g <lucas.trabalho1533@gmail.com>
2022-01-14 16:44:32 +0044

    feat: ensure DbCreateSurvey calls SaveSurveyRepositorywhit correct values

---
+++ src/data/usecases/create-survey/DbCreateSurvey.ts
@@ -0,0 +1,22 @@
+import { CreateSurvey, CreateSurveyModel } from "../../../domain/usecases/CreateSurvey";
+import { SaveSurveyRepository } from "../../protocols/db/survey/SaveSurveyRepository";
+
+export class DbCreateSurvey implements CreateSurvey {
+
+    private readonly saveSurveyRepository: SaveSurveyRepository
+
+
+    constructor(saveSurveyRepository: SaveSurveyRepository) {
+        this.saveSurveyRepository = saveSurveyRepository
+
+    }
+
+    async create(surveyData: CreateSurveyModel): Promise<void> {
+
+        await this.saveSurveyRepository.save(surveyData)
+
+
+
+        return Promise.resolve()
+    }
+}
