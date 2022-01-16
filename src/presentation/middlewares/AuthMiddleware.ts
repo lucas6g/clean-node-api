211188d887acb28de843d7385cd6364962d683bf
lucas6g <lucas.trabalho1533@gmail.com>
2022-01-16 14:33:33 -0300

    feat: ensure AuthMiddleware calls LoadAccontByTokenRepository whit correct middleware

--- src/presentation/middlewares/AuthMiddleware.ts
+++ src/presentation/middlewares/AuthMiddleware.ts
@@ -1,3 +1,4 @@
+import { LoadAccountByTokenRepository } from "../../data/protocols/db/account/LoadAccountByTokenRepository";
 import { AccessDaniedError } from "../errors/AccessDaniedError";
 import { forbidden } from "../helpers/http/httpHelper";
 import { HttpRequest } from "../protocols/HttpRequest";
@@ -5,7 +6,22 @@ import { HttpResponse } from "../protocols/HttpResponse";
 import { Middleware } from "../protocols/Middleware";
 
 export class AuthMiddleware implements Middleware {
+
+    private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
+
+    constructor(loadAccountByTokenRepository: LoadAccountByTokenRepository) {
+        this.loadAccountByTokenRepository = loadAccountByTokenRepository
+    }
+
     async handle(request: HttpRequest): Promise<HttpResponse> {
+
+        const token = request.headers?.['x-access-token']
+        if (token) {
+
+            await this.loadAccountByTokenRepository.getByToken(token)
+        }
+
+
         return await Promise.resolve(forbidden(new AccessDaniedError()))
     }
 }
