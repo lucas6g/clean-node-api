65dcb67fa621d1eba7bcb1ab186aa30b91112618
lucas6g <lucas.trabalho1533@gmail.com>
2022-01-19 08:03:00 -0300

    feat: ensure DbLoadAccountByToken calls LoadAccountByTokenRepository  whit correct values

--- src/data/usecases/load-account-by-token/DbLoadAccountByToken.ts
+++ src/data/usecases/load-account-by-token/DbLoadAccountByToken.ts
@@ -1,19 +1,24 @@
 import { Account } from "../../../domain/entities/Account";
 import { LoadAccountByToken } from "../../../domain/usecases/LoadAccountByToken";
 import { TokenVerifier } from "../../protocols/cryptography/TokenVerifier";
+import { LoadAccountByTokenRepository } from "../../protocols/db/account/LoadAccountByTokenRepository";
 
 
 export class DbLoadAccountByToken implements LoadAccountByToken {
 
     private readonly tokenVerifier: TokenVerifier
+    private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
 
-    constructor(tokenVerifier: TokenVerifier) {
+    constructor(tokenVerifier: TokenVerifier, loadAccountByTokenRepository: LoadAccountByTokenRepository) {
+        this.loadAccountByTokenRepository = loadAccountByTokenRepository
         this.tokenVerifier = tokenVerifier
     }
 
     async getByToken(token: string, role?: string): Promise<Account | null> {
 
         await this.tokenVerifier.verify(token)
+
+        this.loadAccountByTokenRepository.loadByToken(token, role)
 
         return Promise.resolve(null)
     }
