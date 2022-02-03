d733a0f7967967d461823d14a13ddbad73feab7e
lucas6g <lucas.trabalho1533@gmail.com>
2022-02-03 15:22:15 -0300

    test: emsure create survey route retuns 403 if no access token is provided

--- src/main/routes/survey.routes.ts
+++ src/main/routes/survey.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express'
+import { adpteMiddleware } from '../adpters/express/expressMiddlewareAdpter'
 import { adpteRoute } from '../adpters/express/expressRouteAdpter'
 import { makeCreateSurveyController } from '../factories/controllers/createSurveyController/makeCreateSurveyController'
+import { makeAuthMiddleware } from '../factories/middlewares/makeAuthMiddleware'
 
 export default function loginRoutes(router: Router): Router {
-    router.post('/survey', adpteRoute(makeCreateSurveyController()))
+
+
+    router.post('/survey', adpteMiddleware(makeAuthMiddleware('admin')), adpteRoute(makeCreateSurveyController()))
 
     return router
 }
