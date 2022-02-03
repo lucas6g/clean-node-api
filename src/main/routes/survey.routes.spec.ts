badbe805598a44ddaacd2e0dce937810d6d93972
lucas6g <lucas.trabalho1533@gmail.com>
2022-02-03 15:47:00 -0300

    feat: emsure create survey route retuns 204 if valid access token is provided

--- src/main/routes/survey.routes.spec.ts
+++ src/main/routes/survey.routes.spec.ts
@@ -1,13 +1,20 @@
 
 
+
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
+
 import request from 'supertest'
 
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { app } from '../config/app'
+import env from '../config/env'
+
 
 describe('Survey Routes', () => {
     let surveyCollection: Collection
+    let accountCollection: Collection
+    let token: string
 
     beforeAll(async () => {
         await MongoHelper.connect(String(process.env.MONGO_URL))
@@ -19,12 +26,35 @@ describe('Survey Routes', () => {
 
     beforeEach(async () => {
         surveyCollection = await MongoHelper.getCollection('surveys')
+        accountCollection = await MongoHelper.getCollection('accounts')
+
+        const res = await accountCollection.insertOne({
+            name: 'lucas',
+            email: 'lucas@gmail.com',
+            password: '12345',
+            token: 'token',
+            role: 'admin'
+        })
+        const accountId = res.insertedId.toString()
+
+        token = sign({ accountId }, env.jwtSecret)
+
+
+        await accountCollection.updateOne({ _id: accountId }, {
+            $set: {
+                token
+            }
+        })
+
 
 
     })
 
     afterEach(async () => {
         surveyCollection = await MongoHelper.getCollection('surveys')
+        accountCollection = await MongoHelper.getCollection('accounts')
+
+        await accountCollection.deleteMany({})
         await surveyCollection.deleteMany({})
     })
 
@@ -43,5 +73,20 @@ describe('Survey Routes', () => {
             expect(response.status).toBe(403)
         })
     })
+    test('should return 204  survey creation ', async () => {
+        const response = await request(app)
+            .post('/survey')
+            .set('x-access-token', token)
+            .send({
 
+                question: 'anyQuestion',
+                answers: [{
+                    image: 'https://avatars.githubusercontent.com/u/54814274?v=4',
+                    answer: 'anyAnswer'
+                }]
+
+            })
+
+        expect(response.status).toBe(403)
+    })
 })
