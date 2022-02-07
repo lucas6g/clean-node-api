b63a7fd05b6add60df4cd3da3e68e7c3f8aa29f6
lucas6g <lucas.trabalho1533@gmail.com>
2022-02-07 11:57:35 -0300

    refactor: move sut creation to a helper factory method

--- src/presentation/controllers/survey/ListAllSurveysController/ListAllSurveysController.test.ts
+++ src/presentation/controllers/survey/ListAllSurveysController/ListAllSurveysController.test.ts
@@ -3,30 +3,50 @@ import { ListAllSurveysController } from './ListAllSurveysController'
 import { ListAllSurveys } from '../../../../domain/usecases/ListAllSurveys'
 import { Survey } from '../../../../domain/entities/Survey'
 
+const makeListAllSurveysStub = (): ListAllSurveys => {
+    // dependencia mockada
+
+    class ListAllSurveysStub implements ListAllSurveys {
+        async listAll(): Promise<Survey[]> {
+            const surveys: Survey[] = [
+                {
+                    id: 'anyId',
+                    question: 'anyQuestion',
+                    answers: [{
+                        image: 'anyImage',
+                        answer: 'anyAnswer'
+                    }],
+                    date: new Date(),
+                    didAnswer: true
+
+                }
+            ]
+
+            return await Promise.resolve(surveys)
+        }
+    }
+    return new ListAllSurveysStub()
+}
+
+interface SutTypes {
+    sut: ListAllSurveysController
+    listAllSurveysStub: ListAllSurveys
+
+}
+
+const makeSut = (): SutTypes => {
+    const listAllSurveysStub = makeListAllSurveysStub()
+    const sut = new ListAllSurveysController(listAllSurveysStub)
+    return {
+        sut,
+        listAllSurveysStub
+
+    }
+}
+
 describe('ListAllSurveysController', () => {
     test('should call ListAllSurveys whit correct values', async () => {
-        class ListAllSurveysStub implements ListAllSurveys {
-            async listAll(): Promise<Survey[]> {
-                const surveys: Survey[] = [
-                    {
-                        id: 'anyId',
-                        question: 'anyQuestion',
-                        answers: [{
-                            image: 'anyImage',
-                            answer: 'anyAnswer'
-                        }],
-                        date: new Date(),
-                        didAnswer: true
-
-                    }
-                ]
-
-                return await Promise.resolve(surveys)
-            }
-        }
-
-        const listAllSurveysStub = new ListAllSurveysStub()
-        const sut = new ListAllSurveysController(listAllSurveysStub)
+        const { listAllSurveysStub, sut } = makeSut()
 
         const listAllSpy = jest.spyOn(listAllSurveysStub, 'listAll')
 
