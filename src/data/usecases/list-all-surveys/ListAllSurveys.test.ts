8351ba377271c994a0b4b592495d305224b8ca85
lucas6g <lucas.trabalho1533@gmail.com>
2022-02-08 14:35:48 -0300

    test: move sut creation to a helper facotry method

--- src/data/usecases/list-all-surveys/ListAllSurveys.test.ts
+++ src/data/usecases/list-all-surveys/ListAllSurveys.test.ts
@@ -1,31 +1,50 @@
 import { Survey } from '../../../domain/entities/Survey'
+import { ListAllSurveys } from '../../../domain/usecases/ListAllSurveys'
 import { LoadAllSurveysRepository } from '../../protocols/db/survey/LoadAllSurveysRepository'
 import { DbListAllSurveys } from './DbListAllSurveys'
 
+const makeLoadAllSurveysRepositoryStub = (): LoadAllSurveysRepository => {
+    class LoadAllSurveysRepositoryStub implements LoadAllSurveysRepository {
+        async loadAll(): Promise<Survey[]> {
+            const surveys: Survey[] = [
+                {
+                    id: 'anyId',
+                    question: 'anyQuestion',
+                    answers: [{
+                        image: 'anyImage',
+                        answer: 'anyAnswer'
+                    }],
+                    date: new Date(2022, 1, 7, 14),
+                    didAnswer: true
+
+                }
+            ]
+            return await Promise.resolve(surveys)
+        }
+    }
+    return new LoadAllSurveysRepositoryStub()
+}
+
+interface SutTypes {
+    sut: ListAllSurveys
+    loadAllSurveysRepositoryStub: LoadAllSurveysRepository
+
+}
+const makeSut = (): SutTypes => {
+    const loadAllSurveysRepositoryStub = makeLoadAllSurveysRepositoryStub()
+
+    const sut = new DbListAllSurveys(loadAllSurveysRepositoryStub)
+
+    return {
+        sut,
+        loadAllSurveysRepositoryStub
+
+    }
+}
+
 describe('DbListAllSurveys', () => {
     test('should call LoadAllSurveysRepository', async () => {
-        class LoadAllSurveysRepositoryStub implements LoadAllSurveysRepository {
-            async loadAll(): Promise<Survey[]> {
-                const surveys: Survey[] = [
-                    {
-                        id: 'anyId',
-                        question: 'anyQuestion',
-                        answers: [{
-                            image: 'anyImage',
-                            answer: 'anyAnswer'
-                        }],
-                        date: new Date(2022, 1, 7, 14),
-                        didAnswer: true
-
-                    }
-                ]
-                return await Promise.resolve(surveys)
-            }
-        }
-
-        const loadAllSurveysRepositoryStub = new LoadAllSurveysRepositoryStub()
-
-        const sut = new DbListAllSurveys(loadAllSurveysRepositoryStub)
+        const { loadAllSurveysRepositoryStub, sut } = makeSut()
 
         const loadAllSpy = jest.spyOn(loadAllSurveysRepositoryStub, 'loadAll')
 
