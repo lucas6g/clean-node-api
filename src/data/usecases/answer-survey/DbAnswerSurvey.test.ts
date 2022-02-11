7ef9e1376587b761af9bb88c5f7a68696914a44a
lucas6g <lucas.trabalho1533@gmail.com>
2022-02-11 18:13:07 -0300

    test: move sut creation to a helper factory helper method

--- src/data/usecases/answer-survey/DbAnswerSurvey.test.ts
+++ src/data/usecases/answer-survey/DbAnswerSurvey.test.ts
@@ -2,23 +2,42 @@
 import { DbAnswerSurvey } from './DbAnswerSurvey'
 import { LoadSurveyByIdRepository } from '../../protocols/db/survey/LoadSurveyByIdRepository'
 import { SurveyResult } from '../../../domain/entities/SurveyResult'
+import { AnswerSurvey } from '../../../domain/usecases/AnswerSurvey'
+
+const makeLoadSurveyByIdRepositoryStub = (): LoadSurveyByIdRepository => {
+    class LoadSurveyByIdRepositoryStub implements LoadSurveyByIdRepository {
+        async loadById(surverId: string): Promise<SurveyResult | null> {
+            return await Promise.resolve({
+                accountId: 'anyAccountId',
+                answer: 'anyAnswer',
+                date: new Date(2022, 1, 7, 14),
+                id: 'anyId',
+                surveyId: 'anySurveyId'
+            })
+        }
+    }
+    return new LoadSurveyByIdRepositoryStub()
+}
+
+type SutTypes = {
+    sut: AnswerSurvey
+    loadSurveyByIdRepositoryStub: LoadSurveyByIdRepository
+}
+
+const makeSut = (): SutTypes => {
+    const loadSurveyByIdRepositoryStub = makeLoadSurveyByIdRepositoryStub()
+    const sut = new DbAnswerSurvey(loadSurveyByIdRepositoryStub)
+
+    return {
+        sut,
+        loadSurveyByIdRepositoryStub
+
+    }
+}
 
 describe('DbAnswerSurvey', () => {
     test('should call LoadSurveyById whit correct id', async () => {
-        class LoadSurveyByIdRepositoryStub implements LoadSurveyByIdRepository {
-            async loadById(surverId: string): Promise<SurveyResult | null> {
-                return await Promise.resolve({
-                    accountId: 'anyAccountid',
-                    answer: 'anyAnswer',
-                    date: new Date(2022, 1, 7, 14),
-                    id: 'anyId',
-                    surveyId: 'anySurveyId'
-                })
-            }
-        }
-
-        const loadSurveyByIdRepositoryStub = new LoadSurveyByIdRepositoryStub()
-        const sut = new DbAnswerSurvey(loadSurveyByIdRepositoryStub)
+        const { loadSurveyByIdRepositoryStub, sut } = makeSut()
 
         const loadByIdSpy = jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById')
 
