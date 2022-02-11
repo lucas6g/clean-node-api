be64c4eab45901ca3a597c7fa52d4b0da06cfd3a
lucas6g <lucas.trabalho1533@gmail.com>
2022-02-11 09:17:27 -0300

    feat: ensure GET on  /survey returns 204 if theres no surveys and returns 200 if theres surveys~

--- src/main/routes/survey.routes.ts
+++ src/main/routes/survey.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { adpteMiddleware } from '../adpters/express/expressMiddlewareAdpter'
 import { adpteRoute } from '../adpters/express/expressRouteAdpter'
 import { makeCreateSurveyController } from '../factories/controllers/createSurveyController/makeCreateSurveyController'
+import { makeListAllSurveysController } from '../factories/controllers/getAllSurveysController/makeListAllSurveysController'
 import { makeAuthMiddleware } from '../factories/middlewares/makeAuthMiddleware'
 import { makePermitionsMiddleware } from '../factories/middlewares/makePermitionsMiddleware'
 
@@ -9,7 +10,7 @@ export default function loginRoutes(router: Router): Router {
     router
         .post('/survey', adpteMiddleware(makeAuthMiddleware()), adpteMiddleware(makePermitionsMiddleware('admin')), adpteRoute(makeCreateSurveyController()))
     router
-        .get('/survey', adpteMiddleware(makeAuthMiddleware()), adpteRoute(makeCreateSurveyController()))
+        .get('/survey', adpteMiddleware(makeAuthMiddleware()), adpteRoute(makeListAllSurveysController()))
 
     return router
 }
