8cfe5eea0599c6eace431217bfb311a70af30fc5
lucas6g <lucas.trabalho1533@gmail.com>
2022-02-26 00:19:34 -0300

    feat: ensure AnswerSurveyController calls validation whit correct value

--- src/presentation/controllers/survey/AnswerSurveyController/AnswerSurveyController.test.ts
+++ src/presentation/controllers/survey/AnswerSurveyController/AnswerSurveyController.test.ts
@@ -55,4 +55,16 @@ describe('AnswerSurveyController', () => {
         expect(validationSpy).toHaveBeenCalledWith(httpRequest.body)
         expect(validationSpy).toHaveBeenCalledWith(httpRequest.params)
     })
+    test('should return 400 if Validation fails', async () => {
+        const { sut, validationStub } = makeSut()
+
+        const httpRequest = makeFakeHttpRequest()
+
+        jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+
+        const httpResponse = await sut.handle(httpRequest)
+
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error())
+    })
 })
--- src/presentation/controllers/survey/AnswerSurveyController/AnswerSurveyController.ts
+++ src/presentation/controllers/survey/AnswerSurveyController/AnswerSurveyController.ts
@@ -1,3 +1,4 @@
+import { badRequest } from '../../../helpers/http/httpHelper'
 import { Controller } from '../../../protocols/Controller'
 import { HttpRequest } from '../../../protocols/HttpRequest'
 import { HttpResponse } from '../../../protocols/HttpResponse'
@@ -11,8 +12,19 @@ export class AnswerSurveyController implements Controller {
     }
 
     async handle(request: HttpRequest): Promise<HttpResponse> {
-        this.validation.validate(request.body)
-        this.validation.validate(request.params)
+        let error: Error | null
+
+        error = this.validation.validate(request.params)
+
+        if (error) {
+            return badRequest(error)
+        }
+
+        error = this.validation.validate(request.body)
+
+        if (error) {
+            return badRequest(error)
+        }
 
         return await Promise.resolve({
             body: null,
